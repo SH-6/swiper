3fc29df6a4b2e76cd52cf22c193d744cf360065f
unicorn-zero <doy443841@163.com>
2019-10-24 10:31:07 +0800

    增加了Profile模型

--- user/models.py
+++ user/models.py
@@ -36,3 +36,33 @@ class User(models.Model):
             'avatar': self.avatar,
             'location': self.location,
         }
+
+
+class Profile(models.Model):
+    '''用户资料'''
+    SEX = (
+        ('male', '男性'),
+        ('male', '女性')
+    )
+    LOCTION = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('广州', '广州'),
+        ('深圳', '深圳'),
+        ('兰州', '兰州'),
+        ('西安', '西安'),
+        ('重庆', '重庆'),
+        ('成都', '成都')
+    )
+
+    dating_sex = models.CharField(max_length=16, choices=SEX, verbose_name='匹配的性别')
+    location = models.CharField(max_length=16, choices=LOCTION, verbose_name='目标城市')
+
+    min_distance = models.IntegerField(default=1, verbose_name='最小查找范围')
+    max_distance = models.IntegerField(default=10, verbose_name='最大查找范围')
+    min_dating_age = models.IntegerField(default=18, verbose_name='最小交友年龄')
+    max_dating_age = models.IntegerField(default=50, verbose_name='最大交友年龄')
+
+    vibration = models.BooleanField(default=True, verbose_name='开启震动')
+    only_matche = models.BooleanField(default=True, verbose_name='不让匹配的人看我的相册')
+    auto_play = models.BooleanField(default=True, verbose_name='是否自动播放视频')
