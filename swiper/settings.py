81183f236cdaa44c6965bfa5108c4758b6dd8907
unicorn-zero <doy443841@163.com>
2019-10-25 21:26:51 +0800

    下次命名,本次完成头像上传接口

--- swiper/settings.py
+++ swiper/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     # 'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 
     'user',
 ]
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medias')
